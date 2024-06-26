b08aafc2557cfb83f6b7372e0baf1a5bfb6c7012
Manas Das <manaspratimdas2004@gmail.com>
2024-06-26 10:21:23 +0530

    Adding logging

--- eureka-server/EurekaDiscoveryServer/src/main/java/server/EurekaServerApplication.java
+++ eureka-server/EurekaDiscoveryServer/src/main/java/server/EurekaServerApplication.java
@@ -1,14 +1,21 @@
 package server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 	
+	private static final Logger logger = LogManager.getLogger(EurekaServerApplication.class);
 	public static void main(String[] args) {
+		
+		logger.info("Starting EUREKA-DISCOVERY-SERVER");
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
